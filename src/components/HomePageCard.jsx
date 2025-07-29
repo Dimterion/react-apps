3891dba7ed0dc7cbde1a983b0d881a1abf0704d0
Dmitrii <dimterion59@gmail.com>
2025-07-29 10:50:41 +0200

    add home page cards

---
+++ src/components/HomePageCard.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+
+const HomePageCard = ({ link, title }) => {
+  return (
+    <Link
+      to={link}
+      className="flex h-30 w-30 items-center justify-center border p-4 text-center shadow-md hover:shadow-lg"
+    >
+      {title}
+    </Link>
+  );
+};
+
+export default HomePageCard;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router";
+import HomePageCard from "../components/HomePageCard";
 
 const HomePage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="text-2xl font-bold">React Apps</h1>
-      <section className="flex gap-4 text-lg">
-        <Link to="/shopping-cart">Shopping Cart</Link>
-        <Link to="/notes-app">Notes App</Link>
-        <Link to="/rating">Rating</Link>
-        <Link to="/timer">Timer</Link>
+      <section className="mt-4 flex gap-4 text-lg">
+        <HomePageCard link="/shopping-cart" title="Shopping Cart" />
+        <HomePageCard link="/notes-app" title="Notes App" />
+        <HomePageCard link="/rating" title="Rating" />
+        <HomePageCard link="/timer" title="Timer" />
       </section>
     </main>
   );
