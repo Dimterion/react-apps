bcf9d0bd088e88a398e8e682c8c31499c8260787
Dmitrii <dimterion59@gmail.com>
2025-07-29 10:55:00 +0200

    adjust header and home page elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,26 @@ import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between">
+    <header className="flex items-center justify-between border-b border-gray-200 p-2">
       <Link to="/" className="text-xl font-bold">
         React Apps
       </Link>
       <nav className="flex flex-wrap gap-2">
-        <Link to="/">Home</Link>
-        <Link to="/shopping-cart">Shopping Cart</Link>
-        <Link to="/notes-app">Notes App</Link>
-        <Link to="/rating">Rating</Link>
-        <Link to="/timer">Timer</Link>
+        <Link to="/" className="hover:text-gray-500">
+          Home
+        </Link>
+        <Link to="/shopping-cart" className="hover:text-gray-500">
+          Shopping Cart
+        </Link>
+        <Link to="/notes-app" className="hover:text-gray-500">
+          Notes App
+        </Link>
+        <Link to="/rating" className="hover:text-gray-500">
+          Rating
+        </Link>
+        <Link to="/timer" className="hover:text-gray-500">
+          Timer
+        </Link>
       </nav>
     </header>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="text-2xl font-bold">React Apps</h1>
-      <section className="mt-4 flex gap-4 text-lg">
+      <section className="mt-4 flex flex-wrap justify-center gap-4 text-lg">
         <HomePageCard link="/shopping-cart" title="Shopping Cart" />
         <HomePageCard link="/notes-app" title="Notes App" />
         <HomePageCard link="/rating" title="Rating" />
