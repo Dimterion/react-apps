463e13be4c3b5467cd6ec8ff8ad0e175ee5deffb
Dmitrii <dimterion59@gmail.com>
2025-07-29 11:08:25 +0200

    minor elements adjustments

--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,5 +1,5 @@
 const TimerDisplay = ({ time }) => {
-  return <h2 className="mt-4 text-4xl font-semibold">⏳ Timer: {time}</h2>;
+  return <h2 className="my-4 text-4xl font-semibold">⏳ Timer: {time}</h2>;
 };
 
 export default TimerDisplay;
