f337dc8dc83295b859cddd0503f4cc9823a6b8bc
Dmitrii <dimterion59@gmail.com>
2025-08-01 10:40:46 +0200

    adjust not-found page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router";
+
 const NotFoundPage = () => {
-  return <>Page Not Found</>;
+  return (
+    <main className="mx-auto mt-20 flex w-xl max-w-[90vw] flex-col gap-4 text-center">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-xl">This page does not exist.</p>
+      <Link
+        to="/"
+        className="mx-auto w-sm max-w-[80vw] border px-4 py-2 hover:bg-gray-100"
+      >
+        Home page
+      </Link>
+    </main>
+  );
 };
 
 export default NotFoundPage;
