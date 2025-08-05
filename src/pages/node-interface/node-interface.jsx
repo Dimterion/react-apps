6785420c63c1a0f90e819bcb4bb6dd19b7582082
Dmitrii <dimterion59@gmail.com>
2025-08-05 11:26:11 +0200

    adjust NodeInterfacePage elements

--- src/pages/node-interface/node-interface.jsx
+++ src/pages/node-interface/node-interface.jsx
@@ -1,7 +1,19 @@
 import "./node-interface.css";
 
 const NodeInterfacePage = () => {
-  return <main className="nodeInterface-main">Node</main>;
+  return (
+    <main className="nodeInterface-main">
+      <div class="hexagon">
+        <div class="hexagon-inner-1">
+          <div class="hexagon-inner-2">
+            <div class="hexagon-inner-3">
+              <div class="circle"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default NodeInterfacePage;
