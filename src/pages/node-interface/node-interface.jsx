642ee990b02afd4ce0cba5653a265e162c4a0ca9
Dmitrii <dimterion59@gmail.com>
2025-08-07 09:19:51 +0200

    adjust node page elements

--- src/pages/node-interface/node-interface.jsx
+++ src/pages/node-interface/node-interface.jsx
@@ -3,6 +3,23 @@ import "./node-interface.css";
 const NodeInterfacePage = () => {
   return (
     <main className="nodeInterface-main">
+      <div className="terminal">
+        <div className="terminal-header">
+          <div className="terminal-circle bg-red-500"></div>
+          <div className="terminal-circle bg-yellow-500"></div>
+          <div className="terminal-circle bg-green-500"></div>
+          <div className="terminal-console">~/dev/unix</div>
+        </div>
+        <div className="terminal-main">
+          <div className="terminal-output"></div>
+          <div className="terminal-output"></div>
+          <div className="terminal-output"></div>
+          <div className="terminal-output"></div>
+          <div className="terminal-output"></div>
+          <div className="terminal-output"></div>
+          <div className="terminal-output"></div>
+        </div>
+      </div>
       <div className="hexagon">
         <div className="hexagon-inner-1">
           <div className="hexagon-inner-2">
