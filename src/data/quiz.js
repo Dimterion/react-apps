21cfd086098a46af415c253ae763a9ea2600126f
Dmitrii <dimterion59@gmail.com>
2025-09-12 12:46:41 +0200

    adjust quiz style, add more quiz variants

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -17,4 +17,47 @@ export const quiz = [
       { id: 3, answer: "Single Soft Gap", correct: false },
     ],
   },
+  {
+    id: 3,
+    question: "What is JSX?",
+    answers: [
+      { id: 1, answer: "Syntax extension for JavaScript", correct: true },
+      { id: 2, answer: "React native files format", correct: false },
+      {
+        id: 3,
+        answer: "Previous version of JavaScript files formatting",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is Short Circuit Evaluation?",
+    answers: [
+      {
+        id: 1,
+        answer: "A way to address production error in development",
+        correct: false,
+      },
+      {
+        id: 2,
+        answer: "Error appearing after compiling JS-code",
+        correct: false,
+      },
+      {
+        id: 3,
+        answer: "Conditionally rendering elements without if-statement (&&)",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Which one is not a reserved word in JSX?",
+    answers: [
+      { id: 1, answer: "Class", correct: false },
+      { id: 2, answer: "Alt", correct: true },
+      { id: 3, answer: "HTML", correct: false },
+    ],
+  },
 ];
