0238b4f2e087f803732d41ad609bd9163a5cab7d
Dmitrii <dimterion59@gmail.com>
2025-09-12 12:16:39 +0200

    adjust quiz card page style

---
+++ src/data/quiz.js
@@ -0,0 +1,20 @@
+export const quiz = [
+  {
+    id: 1,
+    question: "What is SPA?",
+    answers: [
+      { id: 1, answer: "Single Page Application", correct: true },
+      { id: 2, answer: "Server Process Adjustment", correct: false },
+      { id: 3, answer: "Semi Protected Authentication", correct: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "What is SSG?",
+    answers: [
+      { id: 1, answer: "Sign-in Server Generics", correct: false },
+      { id: 2, answer: "Static Site Generation", correct: true },
+      { id: 3, answer: "Single Soft Gap", correct: false },
+    ],
+  },
+];
--- src/pages/quiz/quiz.jsx
+++ src/pages/quiz/quiz.jsx
@@ -1,29 +1,9 @@
 import { useState } from "react";
 import QuizCard from "../../components/QuizCard/QuizCard";
+import { quiz } from "../../data/quiz";
 import "./quiz.css";
 
 const QuizPage = () => {
-  const quiz = [
-    {
-      id: 1,
-      question: "What is SPA?",
-      answers: [
-        { id: 1, answer: "Single Page Application", correct: true },
-        { id: 2, answer: "Server Process Adjustment", correct: false },
-        { id: 3, answer: "Semi Protected Authentication", correct: false },
-      ],
-    },
-    {
-      id: 2,
-      question: "What is SSG?",
-      answers: [
-        { id: 1, answer: "Sign-in Server Generics", correct: false },
-        { id: 2, answer: "Static Site Generation", correct: true },
-        { id: 3, answer: "Single Soft Gap", correct: false },
-      ],
-    },
-  ];
-
   const [selectedAnswers, setSelectedAnswers] = useState({});
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState(false);
@@ -56,19 +36,21 @@ const QuizPage = () => {
 
   return (
     <main className="quizPage-container">
-      {quiz.map((q) => (
-        <QuizCard
-          key={q.id}
-          questionId={q.id}
-          question={q.question}
-          answers={q.answers}
-          selectedAnswer={selectedAnswers[q.id]}
-          onSelect={(answer) => handleSelect(q.id, answer)}
-          showResult={submitted}
-          submitted={submitted}
-        />
-      ))}
-      <section>
+      <section className="quizPage-quizSection">
+        {quiz.map((q) => (
+          <QuizCard
+            key={q.id}
+            questionId={q.id}
+            question={q.question}
+            answers={q.answers}
+            selectedAnswer={selectedAnswers[q.id]}
+            onSelect={(answer) => handleSelect(q.id, answer)}
+            showResult={submitted}
+            submitted={submitted}
+          />
+        ))}
+      </section>
+      <section className="quizPage-btnSection">
         <button onClick={handleSubmit}>Submit</button>
         {submitted && (
           <>
