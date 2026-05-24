3df20ececc213146c6a8bf79f6bc953b2f98a8a9
Dmitrii <dimterion59@gmail.com>
2026-05-24 10:41:49 +0200

    add cameraUi-topRightSection

--- src/pages/camera-ui/camera-ui.jsx
+++ src/pages/camera-ui/camera-ui.jsx
@@ -3,17 +3,23 @@ import "./camera-ui.css";
 export default function CameraUiPage() {
   return (
     <main className="cameraUi-main">
-      <section className="cameraUi-topLeftSection">
-        <div className="battery-section">
-          <div className="battery-wrapper">
-            <div className="battery-head"></div>
-            <div className="battery-body">
-              <div className="battery-charge"></div>
+      <section className="cameraUi-topSection">
+        <section className="cameraUi-topLeftSection">
+          <div className="battery-section">
+            <div className="battery-wrapper">
+              <div className="battery-head"></div>
+              <div className="battery-body">
+                <div className="battery-charge"></div>
+              </div>
             </div>
+            <div className="battery-time">45min</div>
           </div>
-          <div className="battery-time">45min</div>
-        </div>
-        <div className="camera-resolution">HDV1080i</div>
+          <div className="camera-resolution">HDV1080i</div>
+        </section>
+        <section className="cameraUi-topRightSection">
+          <div className="play-icon"></div>
+          <div>00:01:02:03</div>
+        </section>
       </section>
     </main>
   );
