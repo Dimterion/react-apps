9cd49da1092680391157807e0d074fd3688ff25c
Dmitrii <dimterion59@gmail.com>
2026-05-24 10:27:16 +0200

    add camera-ui page battery elements

--- src/pages/camera-ui/camera-ui.jsx
+++ src/pages/camera-ui/camera-ui.jsx
@@ -3,11 +3,14 @@ import "./camera-ui.css";
 export default function CameraUiPage() {
   return (
     <main className="cameraUi-main">
-      <div className="battery-wrapper">
-        <div className="battery-head"></div>
-        <div className="battery-body">
-          <div className="battery-charge"></div>
+      <div className="battery-section">
+        <div className="battery-wrapper">
+          <div className="battery-head"></div>
+          <div className="battery-body">
+            <div className="battery-charge"></div>
+          </div>
         </div>
+        <div className="battery-time">45min</div>
       </div>
     </main>
   );
