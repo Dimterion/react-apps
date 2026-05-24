f9d2889eb3983ab16465525a1ce87747f31f33de
Dmitrii <dimterion59@gmail.com>
2026-05-24 10:32:37 +0200

    adjust camera-ui top-left section

--- src/pages/camera-ui/camera-ui.jsx
+++ src/pages/camera-ui/camera-ui.jsx
@@ -3,15 +3,18 @@ import "./camera-ui.css";
 export default function CameraUiPage() {
   return (
     <main className="cameraUi-main">
-      <div className="battery-section">
-        <div className="battery-wrapper">
-          <div className="battery-head"></div>
-          <div className="battery-body">
-            <div className="battery-charge"></div>
+      <section className="cameraUi-topLeftSection">
+        <div className="battery-section">
+          <div className="battery-wrapper">
+            <div className="battery-head"></div>
+            <div className="battery-body">
+              <div className="battery-charge"></div>
+            </div>
           </div>
+          <div className="battery-time">45min</div>
         </div>
-        <div className="battery-time">45min</div>
-      </div>
+        <div className="camera-resolution">HDV1080i</div>
+      </section>
     </main>
   );
 }
