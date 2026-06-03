50f2e46fafb4bef4197d033efee4677efafa1c43
Dmitrii <dimterion59@gmail.com>
2026-06-03 10:22:21 +0200

    adjust game-ui page elements & style

--- src/pages/game-ui/game-ui.jsx
+++ src/pages/game-ui/game-ui.jsx
@@ -3,8 +3,25 @@ import "./game-ui.css";
 export default function GameUiPage() {
   return (
     <main className="game-ui">
-      <section className="game-ui__top"></section>
-      <section className="game-ui__bottom"></section>
+      <section className="game-ui__top">
+        <section className="game-ui__group">
+          <span className="game-ui__icon">A</span>
+        </section>
+        <section className="game-ui__group">
+          <span className="game-ui__icon">A</span>
+        </section>
+      </section>
+      <section className="game-ui__bottom">
+        <section className="game-ui__group">
+          <span className="game-ui__icon">A</span>
+        </section>
+        <section className="game-ui__group">
+          <span className="game-ui__icon">A</span>
+        </section>
+        <section className="game-ui__group">
+          <span className="game-ui__icon">A</span>
+        </section>
+      </section>
     </main>
   );
 }
