002135b142c5ea6ae22d82b54ff5f58215ea7eb5
Dmitrii <dimterion59@gmail.com>
2026-06-03 10:18:44 +0200

    adjust game-ui page elements & style

--- src/pages/game-ui/game-ui.jsx
+++ src/pages/game-ui/game-ui.jsx
@@ -1,5 +1,10 @@
 import "./game-ui.css";
 
 export default function GameUiPage() {
-  return <main className="game-ui"></main>;
+  return (
+    <main className="game-ui">
+      <section className="game-ui__top"></section>
+      <section className="game-ui__bottom"></section>
+    </main>
+  );
 }
