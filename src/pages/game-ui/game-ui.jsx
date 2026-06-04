621af8c0e2bf08f1f54a9df645776d01eefa0cb2
Dmitrii <dimterion59@gmail.com>
2026-06-04 09:50:59 +0200

    adjust game-ui page elements & style

--- src/pages/game-ui/game-ui.jsx
+++ src/pages/game-ui/game-ui.jsx
@@ -16,13 +16,13 @@ export default function GameUiPage() {
       </section>
       <section className="game-ui__bottom">
         <section className="game-ui__group">
-          <div className="game-ui__icon"></div>
+          <div className="game-ui__icon game-ui__icon-info"></div>
         </section>
         <section className="game-ui__group">
-          <div className="game-ui__icon"></div>
+          <div className="game-ui__icon game-ui__icon--skill"></div>
         </section>
         <section className="game-ui__group">
-          <div className="game-ui__icon"></div>
+          <div className="game-ui__icon game-ui__icon--resources"></div>
         </section>
       </section>
     </main>
